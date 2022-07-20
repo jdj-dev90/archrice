b6f2ddcbeae89d7fbabd6c2b33882fc798f19d16
Jordan <jordan.geltchrobb@monarcglobal.com>
2022-07-21 08:12:14 +1000

    Add lazydocker/lazynpm + toggleterm keybinds

--- .config/nvim/lua/user/toggleterm.lua
+++ .config/nvim/lua/user/toggleterm.lua
@@ -9,7 +9,7 @@ toggleterm.setup({
 	hide_numbers = true,
 	shade_filetypes = {},
 	shade_terminals = true,
-	shading_factor = 2,
+	shading_factor = 1,
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
@@ -38,6 +38,19 @@ end
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal = require("toggleterm.terminal").Terminal
+
+local lazynpm = Terminal:new({ cmd = "lazynpm", hidden = true })
+
+function _LAZYNPM_TOGGLE()
+	lazynpm:toggle()
+end
+
+local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true })
+
+function _LAZYDOCKER_TOGGLE()
+	lazydocker:toggle()
+end
+
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
 function _LAZYGIT_TOGGLE()
--- .config/nvim/lua/user/whichkey.lua
+++ .config/nvim/lua/user/whichkey.lua
@@ -173,7 +173,9 @@ local mappings = {
 
   t = {
     name = "Terminal",
-    n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
+    d = { "<cmd>lua _LAZYDOCKER_TOGGLE()<cr>", "Lazydocker" },
+    n = { "<cmd>lua _LAZYNPM_TOGGLE()<cr>", "Lazynpm" },
+    o = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
     u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
     t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
     p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
